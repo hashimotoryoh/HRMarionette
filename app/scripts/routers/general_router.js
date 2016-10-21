3a3418c93b57d2a6581d779a79f28b0437cf1a59
Ryoh Hashimoto <hashimotoryoh@gmail.com>
2016-10-21 12:08:34 +0900

    routingのbefore/afterをtriggerするよう実装

--- app/scripts/routers/general_router.js
+++ app/scripts/routers/general_router.js
@@ -9,10 +9,51 @@ define([
 
   var GeneralRouter = Marionette.AppRouter.extend({
 
+    /**
+     * @override
+     */
+    _addAppRoute: function(controller, route, methodName)
+    {
+      var method = controller[methodName];
+
+      if (!method) {
+        throw new Marionette.Error('Method "' + methodName + '" was not found on the controller');
+      }
+
+      /**
+       * custom
+       * 遷移の開始/終了をとる為に、_addAppRouteをoverrideしてる
+       */
+      var wrappedMethod = _.wrap(method, function(func) {
+        Commander.mediator.trigger('before:route', {
+          route      : route,
+          methodName : methodName,
+        });
+
+        Array.prototype.shift.apply(arguments);
+        func.apply(this, arguments);
+
+        Commander.mediator.trigger('after:route',{
+          route      : route,
+          methodName : methodName,
+        });
+      }, this);
+      /**
+       * /custom
+       */
+
+      this.route(route, methodName, _.bind(wrappedMethod, controller));
+    },
+
+
     // 全てのRouteに対して、実行するControllerのメソッドを定義する
     appRoutes: {
       ''          : 'index',
+      'error'     : 'error',
       'workspace' : 'workspace',
+
+      // 一番下に書かなきゃいけない
+      '*notFound' : 'error',
     },
 
   });
